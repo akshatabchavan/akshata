258f9bac9ef311b9e0ae5e8b19cafc93e959a404
Admin <Admin@DESKTOP-SLRUHG1>
2022-01-31 00:37:36 -0800

    commit the project

---
+++ src/gitpractice/GitTest.java
@@ -0,0 +1,10 @@
+package gitpractice;
+
+public class GitTest {
+	public static void main(String[] args) {
+		
+		System.out.println("first sample project");
+		System.out.println("first sample project");
+	}
+	
+}
